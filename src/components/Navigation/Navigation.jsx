295bd338516fafe92813cfc7546c7408d383019d
Dmytro Yanko <yanko.dmitriy@gmail.com>
2022-08-27 18:31:43 +0300

    work

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,24 +3,9 @@ import css from './Navigation.module.css';
 
 const Navigation = () => (
   <nav>
-    <NavLink
-      to="/"
-      className={({ isActive }) => (isActive ? css.link : css.activeLink)}
-    >
-      Home
-    </NavLink>
-    <NavLink
-      to="/movies"
-      className={({ isActive }) => (isActive ? css.link : css.activeLink)}
-    >
-      Movies
-    </NavLink>
-    <NavLink
-      to="/movieId"
-      className={({ isActive }) => (isActive ? css.link : css.activeLink)}
-    >
-      MovieDetails
-    </NavLink>
+    <NavLink to="/">Home</NavLink>
+    <NavLink to="/movies">Movies</NavLink>
+    <NavLink to="/movieId">MovieDetails</NavLink>
     {/* <NavLink to="*">Autor</NavLink> */}
   </nav>
 );
