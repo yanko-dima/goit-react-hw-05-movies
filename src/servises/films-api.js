884dc7d029381fc3ea533218fc8edf2120f02dd0
Dmytro Yanko <yanko.dmitriy@gmail.com>
2022-09-15 19:05:08 +0300

    Work

--- src/servises/films-api.js
+++ src/servises/films-api.js
@@ -2,10 +2,15 @@ import axios from 'axios';
 
 const BASE_URL = 'https://developers.themoviedb.org/3/';
 const API_KEY = '31d9568154610e832478c1f68bfe46b9';
+const testUrl =
+  'https://api.themoviedb.org/3/movie/550?api_key=31d9568154610e832478c1f68bfe46b9';
+// .get(`${BASE_URL}tranding/movie/day/?api_key=${API_KEY}&`)
 
 export function getTrendingFilms() {
   return axios
-    .get(`${BASE_URL}tranding/movie/day/?api_key=${API_KEY}&`)
+    .get(
+      `https://api.themoviedb.org/3/movie/550?api_key=31d9568154610e832478c1f68bfe46b9`
+    )
     .then(response => console.log(response));
 }
 
