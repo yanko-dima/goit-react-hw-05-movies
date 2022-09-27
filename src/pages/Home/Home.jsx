f7d7b16dbb1e4a4d0794ba117ad3817554bae77a
Dmytro Yanko <yanko.dmitriy@gmail.com>
2022-09-27 09:38:11 +0300

    work

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
+import { TailSpin } from 'react-loader-spinner';
 import PageHeading from 'components/PageHeading';
 import { getTrendingFilms } from 'servises/films-api';
 import TrandingFilmsList from 'components/TrandingFilmsList';
+import css from './Home.module.css';
 
 export default function Home() {
   const [films, setFilms] = useState([]);
@@ -12,8 +14,22 @@ export default function Home() {
 
   return (
     <>
-      <PageHeading text={'Tranding today'} />
-      <TrandingFilmsList films={films} />
+      {!films && (
+        <TailSpin
+          color="#032541"
+          width="50"
+          height="50"
+          ariaLabel="tail-spin-loading"
+          wrapperClass={css.loaderWrapper}
+        />
+      )}
+
+      {films && (
+        <>
+          <PageHeading text={'Tranding today'} />
+          <TrandingFilmsList films={films} />
+        </>
+      )}
     </>
   );
 }
