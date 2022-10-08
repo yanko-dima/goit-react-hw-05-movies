2b2e35af459ef14d5532fbc565d35c26eb066114
Dmytro Yanko <yanko.dmitriy@gmail.com>
2022-10-08 19:32:56 +0300

    404 page Do

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,18 @@
+import PageHeading from 'components/PageHeading';
+import img from 'img/404.jpg';
+import css from './NotFound.module.css';
+
 export default function NotFound() {
   return (
-    <div>
-      <p>404 Page not found :(</p>
+    <div className={css.notFound}>
+      <img className={css.notFoundImg} src={img} alt="" />
+      <div className={css.notFoundContent}>
+        <PageHeading text={'Awww...Don`t Cry.'} />
+        <p>It`s just 404 Error!</p>
+        <p>
+          What you`re looking for may have been misplaced in Long Term Memory.
+        </p>
+      </div>
     </div>
   );
 }
