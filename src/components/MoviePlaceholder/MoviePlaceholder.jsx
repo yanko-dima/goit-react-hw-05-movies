1de3c4f59e7c2b3db5d32907665a04289d5f8267
Dmytro Yanko <yanko.dmitriy@gmail.com>
2022-10-08 19:36:45 +0300

    fix

--- src/components/MoviePlaceholder/MoviePlaceholder.jsx
+++ src/components/MoviePlaceholder/MoviePlaceholder.jsx
@@ -1,9 +1,10 @@
 import PageHeading from 'components/PageHeading';
+import css from './MoviePlaceholder.module.css';
 
 export default function MoviePlaceholder() {
   return (
-    <>
+    <div className={css.moviePlaceholder}>
       <PageHeading text="Film search service" />
-    </>
+    </div>
   );
 }
